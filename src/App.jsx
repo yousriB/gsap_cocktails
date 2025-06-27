aecfacf07c0ee47423417849916951695a36feb8
yousri <129407665+yousriB@users.noreply.github.com>
2025-06-27 11:18:27 +0100

    v0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-indigo-500 text-4xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
