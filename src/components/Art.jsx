75a4fc2718c1ba73b1c9bd2478ce617a594542ba
yousri <129407665+yousriB@users.noreply.github.com>
2025-07-01 13:32:28 +0100

    Art section is done

---
+++ src/components/Art.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { featureLists, goodLists } from "../../constants";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Art = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const strat = isMobile ? "top 20%" : "top top";
+
+    const maskTiemline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#art",
+        strat,
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    maskTiemline
+      .to(".will-fade", {
+        opacity: 0,
+        stagger: 0.2,
+        ease: "power1.inOut",
+      })
+      .to(".masked-img", {
+        scale: 1.3,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
+  });
+
+  return (
+    <div id="art">
+      <div className="container mx-auto h-full pt-20">
+        <h2 className="will-fade">The ART</h2>
+
+        <div className="content">
+          <ul className="space-y-4 will-fade">
+            {goodLists.map((feature, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p>{feature}</p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="cocktail-img">
+            <img
+              src="/images/under-img.jpg"
+              alt="cocktail"
+              className="abs-center masked-img size-full object-contain"
+            />
+          </div>
+          <ul className="space-y-4 will-fade">
+            {featureLists.map((feature, index) => (
+              <li key={index} className="flex items-center justify-start gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p className="md:w-fit w-60">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="masked-container">
+          <h2 className="will-fade">Sip-Worthy Perfection</h2>
+          <div id="masked-content">
+            <h3>Made with Craft, Poured with Passion</h3>
+            <p>
+              This isn’t just a drink. It’s a carefully crafted moment made just
+              for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Art;
